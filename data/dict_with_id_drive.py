ff5ef6480d460d81afad9737f3fd50fb0b067d8c
David Castellano <david@beiota.com>
2021-02-28 21:13:36 +0100

    Update dict_with_id_drive.py

--- data/dict_with_id_drive.py
+++ data/dict_with_id_drive.py
@@ -1,4 +1,4 @@
 class id_drive_to_download:
     ids_dataset={"damagenet":"1K2C38bWRCeuNRtWW6qbAF8xFjxB-969U",
                  "imagenet_validation":"1QA2HeOxusHicdbUxg54M8PG2GorB8ukG",
-                "David_image_perturbedpertBig_mod_3_round2_0025":"1xJ8Vzack9i1v4vuiCc9KS2VyDYKUCI_c"}
+                "David_image_perturbedpertBig_mod_3_round3_0025":"12ik-XEbM-DaWrNpdZnGFdRaOS-FygqxJ"}
