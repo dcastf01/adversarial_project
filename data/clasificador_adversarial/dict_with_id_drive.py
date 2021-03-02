8a03dabc29f16685323000d0aee9b5d67b25f76f
David Castellano <david@beiota.com>
2021-03-02 19:30:59 +0100

    reorganized

---
+++ data/clasificador_adversarial/dict_with_id_drive.py
@@ -0,0 +1,4 @@
+class id_drive_to_download:
+    ids_dataset={"damagenet":"1K2C38bWRCeuNRtWW6qbAF8xFjxB-969U",
+                 "imagenet_validation":"1QA2HeOxusHicdbUxg54M8PG2GorB8ukG",
+                "David_image_perturbedpertBig_mod_3_round3_0025":"1m-YEiISVsKmGmUVPeou2ul3FrJOqxUrM"}
