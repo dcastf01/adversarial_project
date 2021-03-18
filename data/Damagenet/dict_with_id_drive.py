2af8c2f7e69e3626b67f4da23d1140aeedf663bd
David Castellano <dcastf01@gmail.com>
2021-03-18 09:52:30 +0100

    update downloads

--- data/Damagenet/dict_with_id_drive.py
+++ data/Damagenet/dict_with_id_drive.py
@@ -1,6 +1,6 @@
 class id_drive_to_download:
     ids_dataset={
-                 "imagenet_validation":"1QA2HeOxusHicdbUxg54M8PG2GorB8ukG",
+                 "imagenet_validation.zip":"1QA2HeOxusHicdbUxg54M8PG2GorB8ukG",
               
                 
                 
