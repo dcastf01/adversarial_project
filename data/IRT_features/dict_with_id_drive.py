ba93cc6788b5c677312676ae58defceec70c83d8
David Castellano <dcastf01@gmail.com>
2021-04-07 11:47:23 +0200

    Update dict_with_id_drive.py

--- data/IRT_features/dict_with_id_drive.py
+++ data/IRT_features/dict_with_id_drive.py
@@ -1,6 +1,6 @@
 class id_drive_to_download:
     ids_dataset={
-        "damagenet":"1K2C38bWRCeuNRtWW6qbAF8xFjxB-969U",
+        "damagenet_damagenet.zip":"1K2C38bWRCeuNRtWW6qbAF8xFjxB-969U",
                 "imagenet_validation_0.zip":"1QA2HeOxusHicdbUxg54M8PG2GorB8ukG",
                 "David_image_perturbedpertBig_round7_075.zip": "1__vhYkd-FpNNCIYuLSyyS2huvTOfsAZT",
                 "David_image_perturbedpertBig_round7_05.zip": "19szuYAsFPVA922UpOlypz_0PFX0H7IPa",
